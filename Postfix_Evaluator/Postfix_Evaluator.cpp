21b825cdfb5815ba1511a9a8989699163502253f
aluu275 <anthonyluu275@gmail.com>
2020-09-11 12:16:24 -0700

    Creation of Postfix_Evaluator Folder

---
+++ Postfix_Evaluator/Postfix_Evaluator.cpp
@@ -0,0 +1,1 @@
+#include "Postfix_Evaluator.h"
---
+++ Postfix_Evaluator/Postfix_Evaluator.h
@@ -0,0 +1,34 @@
+#ifndef POSTFIX_EVALUATOR_H
+#define POSTFIX_EVALUATOR_H
+
+#include <iostream>
+#include <exception>
+#include <string>
+#include <stack>
+
+using namespace std;
+
+// ~	Unary Operator
+// !	Factorial Operator
+// +	Addition Operator
+// *	Multiplication Operator
+// -	Subtraction Operator
+// /	Division Operator
+//
+
+float UnaryOp(stack<float> &s)
+{
+
+
+
+}
+
+float Postfix_Evaluator(string uInput)
+{
+	float result;
+
+
+}
+
+#endif
+
